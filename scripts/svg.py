49c9240c2ab8af71b8ca0d5112efcbb54f9ab7f9
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-04-29 10:45:20 +0100

    Added basic logic for editing svg using javascript

--- scripts/svg.py
+++ scripts/svg.py
@@ -7,6 +7,7 @@ from utils import HERE
 
 env = Environment(loader=FileSystemLoader(HERE))
 svg_template = env.get_template("template.svg")
+svg_template = env.get_template("template.html")
 
 
 def write_svg(ydim, xdim, tensor_manager, raster_fname, fh):
@@ -16,3 +17,22 @@ def write_svg(ydim, xdim, tensor_manager, raster_fname, fh):
                                  ydim=ydim,
                                  tensors=tensors,
                                  raster_fname=raster_fname))
+
+def write_html(ydim, xdim, tensor_manager, raster_fname, fh):
+    """Write out HTML with inlined SVG illustration."""
+    tensors = [tensor_manager[i] for i in tensor_manager.identifiers]
+    fh.write(html_template.render(xdim=xdim,
+                                  ydim=ydim,
+                                  tensors=tensors,
+                                  raster_fname=raster_fname))
+
+if __name__ == "__main__":
+    from tensor import TensorManager
+    ydim, xdim = 1362, 836
+    tensors = TensorManager()
+    with open("raw_tensors.txt") as fh:
+        tensors.read_raw_tensors(fh)
+    with open("test.svg", "w") as fh:
+        write_svg(ydim, xdim, tensors, "segmentation.png", fh)
+    with open("test.html", "w") as fh:
+        write_svg(ydim, xdim, tensors, "segmentation.png", fh)
